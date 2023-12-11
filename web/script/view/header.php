d06d25c78bc4fff39147041bdb87dd938ca5adc0
zzh <57039182+zzhhao2008@users.noreply.github.com>
2023-12-11 21:35:31 +0800

    项目初始化
    Project Init

---
+++ web/script/view/header.php
@@ -0,0 +1,17 @@
+<meta charset="UTF-8">
+<link rel="shortcut icon" href="/icon.jpg">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=0.5, maximum-scale=2.0, user-scalable=yes" />
+<link rel="stylesheet" href="/static/bootstrap/5.3.2/css/bootstrap.css">
+<link rel="stylesheet" href="/static/css/public.css">
+<link rel="stylesheet" href="/static/css/tool.css">
+<link rel="stylesheet" href="/static/css/theme/<?=custom::readid()?>.css">
+<link rel="stylesheet" href="/static/bootstrap/icons/bootstrap-icons.css">
+<script src="/static/bootstrap/5.3.2/js/bootstrap.bundle.js"></script>
+<script src="/static/js/view.js"></script>
+<script src="/static/js/src-min/ace.js"></script>
+<script src="/static/js/aceinit.js"></script>
+<style>
+    #<?=Router::getUri()?>naver{
+        border-bottom: 2px solid red;
+    }
+</style>
