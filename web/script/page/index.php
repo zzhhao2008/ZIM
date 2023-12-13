7364bcd3752b5747fb5cd260e170c55649864cdf
zzh <57039182+zzhhao2008@users.noreply.github.com>
2023-12-13 20:50:28 +0800

    RMD

--- web/script/page/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-view::header("主界面");
-?>
--- web/script/route.php
+++
@@ -1,7 +0,0 @@
-<?php
-Router::login("/", "index");
-Router::login("login","index");
-Router::login("logup", "index");
-
-Router::guest("logup", "logup");
-Router::guest("/", "login");
